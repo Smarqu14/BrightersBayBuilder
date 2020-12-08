b6bb130d7847b0d2b938b9f51f543d875f552340
Steve Marquez <steveantonio00@gmail.com>
2020-12-07 19:08:25 -0800

    Add contact logo

--- src/components/contactpage/Contact.js
+++ src/components/contactpage/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import bbbLogo from '../../logos/logo-white.png';
+import bbbLogo from '../../logos/bbb-logo.png';
 import Form from './Form';
 
 const Contact = () => {
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
-// Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: 'AIzaSyBR9-KLUs57jA1evEXNpe5hbiXZ3KGffxc',
   authDomain: 'brighterbaybuilders.firebaseapp.com',
@@ -9,11 +8,7 @@ var firebaseConfig = {
   storageBucket: 'brighterbaybuilders.appspot.com',
   messagingSenderId: '812838614617',
   appId: '1:812838614617:web:f9da610502cd0124a96348',
-  measurementId: 'G-99K9BH37HN',
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 export default firebase;
