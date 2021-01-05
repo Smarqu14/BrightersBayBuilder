9ac5483d6050bce240ff07e6a31685aebe4eb7a4
Steve Marquez <steveantonio00@gmail.com>
2021-01-05 15:06:54 -0800

    Finish services page

--- src/components/servicespage/Services.js
+++ src/components/servicespage/Services.js
@@ -8,15 +8,14 @@ const Services = ({ services }) => {
       <Container className='mb-5'>
         <p className='p-5 paragraph__text'>
           Whether it's a multimillion-dollar technical facility or the
-          renovation of a single office, <strong>Brighter Bay Builders </strong>
-          is committed to exceeding our clients' expectations through a
-          proactive and progressive approach to every project. Our general
-          construction services are performed by staff seasoned veterans who
-          share a commitment to excellence.
-          <strong> Brighter Bay Builders </strong>
-          uses cutting edge construction technology in executing the work and
-          delivers greater value through meticulous planning and conscientious
-          execution.
+          renovation of a single office,{' '}
+          <strong>Brighter Bay Builders, Inc.</strong> is committed to exceeding
+          our clients' expectations through a proactive and progressive approach
+          to every project. Our general construction services are performed by
+          seasoned staff veterans who share a commitment to excellence.{' '}
+          <strong>Brighter Bay Builders, Inc.</strong> uses cutting edge
+          construction technology to execute the work and deliver more excellent
+          value through meticulous planning and conscientious execution.
         </p>
         <ServiceItem services={services} />
       </Container>
