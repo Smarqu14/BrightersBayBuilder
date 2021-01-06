c81b2067f61357f77f6a7778ddfa93eef776ce8e
Steve Marquez <steveantonio00@gmail.com>
2021-01-05 20:49:16 -0800

    Add images for SSF residence

--- src/data.js
+++ src/data.js
@@ -1,12 +1,25 @@
-import slider1 from './images/slider/bathroom.jpg';
-import slider2 from './images/slider/construction1.jpg';
-import slider3 from './images/slider/noeproj1.jpg';
-import slider4 from './images/slider/noevalley2.jpg';
+import slider1 from './images/bathroom.jpg';
+import slider2 from './images/construction1.jpg';
+import slider3 from './images/noeproj1.jpg';
+import slider4 from './images/noevalley2.jpg';
 
-import service1 from './images/servicesImg/services1.jpg';
-import service2 from './images/servicesImg/services2.jpg';
-import service3 from './images/servicesImg/services3.jpg';
-import service4 from './images/servicesImg/services4.jpg';
+import service1 from './images/services1.jpg';
+import service2 from './images/services2.jpg';
+import service3 from './images/services3.jpg';
+import service4 from './images/services4.jpg';
+
+import noe1 from './images/noevalley1.jpg';
+import noe2 from './images/noevalley3.jpg';
+import noe3 from './images/noevalley4.jpg';
+import noe4 from './images/noevalley5.jpg';
+import noe5 from './images/noevalley6.jpg';
+
+import ssf1 from './images/ssf1.jpg';
+import ssf2 from './images/ssf2.jpg';
+import ssf3 from './images/ssf3.jpg';
+import ssf4 from './images/ssf5.jpg';
+import ssf5 from './images/ssf6.jpg';
+import ssf6 from './images/ssf7.jpg';
 
 export const servicesImages = [
   {
@@ -79,165 +92,6 @@ export const aboutData = [
   },
 ];
 
-export const projects = [
-  {
-    _id: 'chinatown-residence',
-    name: 'Chinatown Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/1.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/2.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/3.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/4.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/5.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/6.jpeg',
-    ],
-  },
-  {
-    _id: 'fisherman-residence',
-    name: "Fisherman's Wharf Residence",
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/7.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/8.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/9.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/10.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/11.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/12.jpeg',
-    ],
-  },
-  {
-    _id: 'mission-residence',
-    name: 'Mission District Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/15.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/14.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/15.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/16.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/17.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/18.jpeg',
-    ],
-  },
-  {
-    _id: 'noe-residence',
-    name: 'Noe Valley Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/18.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/19.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/20.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/21.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/22.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/23.jpeg',
-    ],
-  },
-  {
-    _id: 'embarcadero-residence',
-    name: 'Embarcadero Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/24.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/25.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/26.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/27.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/28.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/29.jpeg',
-    ],
-  },
-  {
-    _id: 'castro-residence',
-    name: 'The Castro Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/34.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/31.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/32.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/33.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/30.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/35.jpeg',
-    ],
-  },
-  {
-    _id: 'northbeach-residence',
-    name: 'North Beach Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/36.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/37.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/38.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/39.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/40.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/41.jpeg',
-    ],
-  },
-  {
-    _id: 'sunset-residence',
-    name: 'Sunset District Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/42.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/43.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/44.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/45.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/46.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/47.jpeg',
-    ],
-  },
-  {
-    _id: 'tenderloin-residence',
-    name: 'Tenderloin Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/48.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/49.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/50.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/51.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/52.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/53.jpeg',
-    ],
-  },
-  {
-    _id: 'presidio-residence',
-    name: 'Presidio Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/54.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/55.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/56.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/57.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/58.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/59.jpeg',
-    ],
-  },
-  {
-    _id: 'treasure-residence',
-    name: 'Treasure Island Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/60.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/61.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/62.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/63.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/64.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/65.jpeg',
-    ],
-  },
-  {
-    _id: 'japantown-residence',
-    name: 'Japantown Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/66.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/67.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/68.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/69.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/70.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/71.jpeg',
-    ],
-  },
-  {
-    _id: 'height-residence',
-    name: 'Height-Ashbury Residence',
-    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/76.jpeg',
-    imagesList: [
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/73.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/74.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/72.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/76.jpeg',
-      'https://bbb-construction.s3-us-west-1.amazonaws.com/77.jpeg',
-    ],
-  },
-];
-
 export const services = [
   {
     _id: 'appartment-renovation',
@@ -278,6 +132,45 @@ export const services = [
       'Our builders will make it a reality for you',
       'Start-to-finish full-service contractor',
       'No stress! We expertly navigate regulations in and around high-rise buildings',
+    ],
+  },
+];
+
+export const projects = [
+  {
+    _id: 'noe-residence',
+    name: 'Noe Valley Residence 1',
+    image: noe1,
+    imagesList: [noe1, noe2, noe3, noe4, noe5],
+  },
+  {
+    _id: 'new-Construction',
+    name: 'South San Francisco Residence',
+    image: ssf1,
+    imagesList: [ssf2, ssf3, ssf4, ssf5, ssf6],
+  },
+  {
+    _id: 'mission-residence',
+    name: 'Mission District Residence',
+    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/15.jpeg',
+    imagesList: [
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/14.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/15.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/16.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/17.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/18.jpeg',
+    ],
+  },
+  {
+    _id: 'noe-residence',
+    name: 'Noe Valley Residence',
+    image: 'https://bbb-construction.s3-us-west-1.amazonaws.com/18.jpeg',
+    imagesList: [
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/19.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/20.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/21.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/22.jpeg',
+      'https://bbb-construction.s3-us-west-1.amazonaws.com/23.jpeg',
     ],
   },
 ];
